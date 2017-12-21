0c316ede909ec91662f82cebdd256a614b3d303e
boxadesign <info@boxadesign.co.uk>
2017-12-21 22:17:32 +0000

    web tests for previous version page

---
+++ page_objects/web/previousVersionsPage.js
@@ -0,0 +1,46 @@
+module.exports = {
+    url: (datasetID, edition) => {
+        datasetID = datasetID || "931a8a2a-0dc8-42b6-a884-7b6054ed3b68";
+        edition = edition || "Time-series";
+        return process.env.ROUTER_URL + "/datasets/" + datasetID + "/editions/" + edition + "/versions";
+    },
+    elements: {
+        heading: {
+            selector: "h1 strong",
+            locateStrategy: 'css selector'
+        },
+        latestVersionLink: {
+            selector: ".col > p > a",
+            locateStrategy: 'css selector'
+        },
+        learnMore: {
+            selector: "details summary",
+            locateStrategy: 'css selector'
+        },
+        revisionsDescription: {
+            selector: "details .panel",
+            locateStrategy: 'css selector'
+        },
+        revisionDate: {
+            selector: ".table-preview tr > td",
+            locateStrategy: 'css selector'
+        },
+        revisionReason: {
+            selector: ".table-preview tr td:nth-child(2n)",
+            locateStrategy: 'css selector'
+        },
+        csvFile: {
+            selector: "//*[contains(@class, 'table-preview')]/descendant::a[contains(text(), 'csv')]",
+            locateStrategy: 'xpath'
+        },
+        xlsFile: {
+            selector: "//*[contains(@class, 'table-preview')]/descendant::a[contains(text(), 'xls')]",
+            locateStrategy: 'xpath'
+        }
+    },
+    commands: [{
+        waitForLoad: function() {
+            return this.waitForElementVisible('@heading', 5000);
+        }
+    }]
+}
---
+++ step_definitions/web/previous_versions_page.js
@@ -0,0 +1,77 @@
+var {client} = require('nightwatch-cucumber');
+var {defineSupportCode} = require('cucumber');
+var http = require('http');
+
+var previousVersionsPage = client.page.previousVersionsPage();
+
+defineSupportCode(({Given, Then, When}) => {
+    /*
+    Reused across scenarios
+    */
+    Given(/^I go to the previous versions page/, () => {
+        return previousVersionsPage
+            .navigate()
+            .waitForLoad();
+    });
+
+    /*
+    Return to the latest version
+    */
+    When(/^I click the latest version link/, () => {
+        return previousVersionsPage
+            .click('@latestVersionLink')
+    });
+
+    Then(/^I can see the latest version page/, () => {
+        return previousVersionsPage
+            .waitForLoad()
+            .expect.element('@heading').text.to.equal("Acceptance test");
+    });
+
+    /*
+    Learn more about statistical revisions
+    */
+    When(/^I click the learn more link/, () => {
+        return previousVersionsPage
+            .click('@learnMore');
+    });
+
+    Then(/^I can see the information about revisions/, () => {
+        return previousVersionsPage
+            .waitForElementVisible('@revisionsDescription', 1000)
+            .getAttribute('@revisionsDescription', 'aria-hidden', function(status) {
+                this.assert.equal(status.value, 'false');
+            });
+    });
+
+    /*
+    Revision date is formatted correctly
+    */
+    Then(/^I can see the revision date is formatted/, () => {
+        return previousVersionsPage
+            .expect.element('@revisionDate').text.to.equal('25 December 2017');
+    });
+
+    /*
+    Revision reason conatains information
+    */
+    Then(/^I can see the revision reason is displayed/, () => {
+        return previousVersionsPage
+            .expect.element('@revisionReason').text.to.equal('-');
+    });
+
+    /*
+    Download a version in available formats
+    */
+    Then(/^I can see the csv file to download/, () => {
+        return previousVersionsPage
+            .assert.attributeEquals('@csvFile', 'href', process.env.ROUTER_URL + '/download.csv');
+    });
+
+    Then(/^I can see the xls file to download/, () => {
+        return previousVersionsPage
+            .assert.attributeEquals('@xlsFile', 'href', process.env.ROUTER_URL + '/download.xls');
+    });
+
+
+});
